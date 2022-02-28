49141ade1a1265db7f4225deeb82737e8dcccae7
Ahad hossain <ahadxx99@gmail.com>
2022-03-01 02:59:13 +0800

    first commit

---
+++ src/currency/data.js
@@ -0,0 +1,34 @@
+const loadCurrency = [
+  // {
+  //   currency: 'USD',
+  //   name: 'doller',
+  //   symbol: '$',
+  //   value: 1
+  // },
+  // {
+  //   currency: 'IND',
+  //   name: 'rupees',
+  //   symbol: '₹',
+  //   value: 60.0
+  // },
+  {
+    currency: 'BDT',
+    name: 'taka',
+    symbol: '৳',
+    value: 80.0
+  }
+  // {
+  //   currency: 'EUR',
+  //   name: 'euro',
+  //   symbol: '€',
+  //   value: 52.00
+  // },
+  // {
+  //   currency: 'GBP',
+  //   name: 'pound',
+  //   symbol: '£',
+  //   value: 62.00
+  // }
+]
+
+module.exports = loadCurrency;
